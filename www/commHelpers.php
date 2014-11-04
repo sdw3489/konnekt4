f1aaa515a54d4d54b36a7a249d9379db5cdb146b
Seth Whitaker <sdw3489@gmail.com>
2014-11-04 12:02:21 -0500

    check that query finds rows before returning data

--- www/commHelpers.php
+++ www/commHelpers.php
@@ -7,25 +7,27 @@
 function returnAssArray ($stmt){
   $stmt->execute();
   $stmt->store_result();
-  $meta = $stmt->result_metadata();
-  $bindVarsArray = array();
+  if($stmt->num_rows > 0){
+    $meta = $stmt->result_metadata();
+    $bindVarsArray = array();
 
-	//using the stmt, get it's metadata (so we can get the name of the name=val pair for the associate array)!
-	while ($column = $meta->fetch_field()) {
+  	//using the stmt, get it's metadata (so we can get the name of the name=val pair for the associate array)!
+  	while ($column = $meta->fetch_field()) {
     	$bindVarsArray[] = &$results[$column->name];
     }
 
-	//bind it!
-	call_user_func_array(array($stmt, 'bind_result'), $bindVarsArray);
+  	//bind it!
+  	call_user_func_array(array($stmt, 'bind_result'), $bindVarsArray);
 
-	//now, go through each row returned,
-	while($stmt->fetch()) {
+  	//now, go through each row returned,
+  	while($stmt->fetch()) {
     	$clone = array();
-        foreach ($results as $k => $v) {
-        	$clone[$k] = $v;
-        }
-        $data[] = $clone;
+      foreach ($results as $k => $v) {
+      	$clone[$k] = $v;
+      }
+      $data[] = $clone;
     }
     return json_encode($data);
+  }
 }
 ?>
