86f24e2a69ceafc1a5a9d6bbbc275b4f9456dd76
Seth Whitaker <sdw3489@gmail.com>
2014-11-05 01:35:19 -0500

    delete old images

--- www/_includes/head.php
+++ www/_includes/head.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?= $title; ?></title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
   <link type="text/css" rel="stylesheet" href="/css/styles.css" />
