032fcb7e74ce2caa9be45a2eb08d9d39b6018fff
Seth Whitaker <sdw3489@gmail.com>
2014-11-07 19:43:23 -0500

    global views added

---
+++ www/app/views/global/head.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?= $title; ?> | Konnekt4</title>
+  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="/favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+  <link type="text/css" rel="stylesheet" href="/css/styles.css" />
+</head>
+<body>
