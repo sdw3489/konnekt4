042f46589cc355832514418a7bf872a69f1af61a
Seth Whitaker <sdw3489@gmail.com>
2014-11-21 15:17:36 -0500

    html5 front end validations. Closes #49

--- www/app/views/signup.php
+++ www/app/views/signup.php
@@ -4,8 +4,8 @@
       <h1>Sign Up</h1>
       <p>Already Signed up? <a href="/">Login</a></p>
       <?php echo validation_errors('<p class="text-danger">', '</p>'); ?>
-      <input type="text" name="username" class="signin-name form-control" value="<?php echo set_value('username'); ?>" placeholder="Username" required autofocus autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false">
-      <input type="password" name="password" class="signin-password form-control" value="<?php echo set_value('password'); ?>" placeholder="Password" required autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false">
+      <input type="text" name="username" class="signin-name form-control" value="<?php echo set_value('username'); ?>" placeholder="Username" pattern=".{3,}" title="A minimum of 3 characters is required" required autofocus autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false">
+      <input type="password" name="password" class="signin-password form-control" value="<?php echo set_value('password'); ?>" placeholder="Password" pattern=".{3,}" title="A minimum of 3 characters is required" required autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false">
       <input class="btn btn-lg btn-primary btn-block" type="submit" name="submit-register" value="Sign Up"/>
     </form>
   </div> <!-- /container -->
