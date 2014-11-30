6ddd212a9b90d19c5db0246d5b81556c5bb3dbb1
Seth Whitaker <sdw3489@gmail.com>
2014-11-29 19:48:55 -0500

    Fix Navbar structure. Fixes #50

--- www/app/views/global/nav.php
+++ www/app/views/global/nav.php
@@ -13,26 +13,14 @@
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-      </ul>
-
-      <ul class="nav navbar-nav navbar-right">
-        <li class="navbar-text"><span class="glyphicon glyphicon-user"></span> Signed in as <?=ucfirst($_SESSION['username']);?></li>
-        <li><a href="/user/logout/"><span class="glyphicon glyphicon-off"></span> Logout</a></li>
-       <!--  <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Account &amp; Support <b class="caret"></b></a>
-          <ul class="dropdown-menu">
-            <li class="dropdown-header">Account</li>
-            <li><a href="#"><span class="glyphicon glyphicon-user"></span> My Account</a></li>
-            <li class="divider"></li>
-            <li class="dropdown-header">Support</li>
-            <li><a href="#"><span class="glyphicon glyphicon-question-sign"></span> Support</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-info-sign"></span> FAQs</a></li>
-            <li class="divider"></li>
-            <li><a href="/app/controllers/userController.php?logout"><span class="glyphicon glyphicon-off"></span> Logout</a></li>
-          </ul>
-        </li> -->
-      </ul>
+      <div class="navbar-right">
+        <p class="navbar-text">
+          <span class="glyphicon glyphicon-user"></span> Signed in as <?=ucfirst($_SESSION['username']);?>
+        </p>
+        <ul class="nav navbar-nav">
+          <li><a href="/user/logout/"><span class="glyphicon glyphicon-off"></span> Logout</a></li>
+        </ul>
+      </div>
 
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
