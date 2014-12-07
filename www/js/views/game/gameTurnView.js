2979b5bfa1ac4c8f2d20fd736ddc8819776b82e1
Seth Whitaker <sdw3489@gmail.com>
2014-12-07 18:21:37 -0500

    revert back to previous style of player/turn info. added colored piece legend instead.

--- www/js/views/game/gameTurnView.js
+++ www/js/views/game/gameTurnView.js
@@ -10,6 +10,8 @@ define([
     tagName:'li',
     className: 'list-group-item',
     template:_.template(TurnTemplate),
+    yourClass : 'list-group-item-success',
+    oppClass : 'list-group-item-danger',
     yourText : 'Your Turn',
     oppText : 'Opponents Turn',
 
@@ -19,12 +21,12 @@ define([
     render: function () {
       if(this.model.get('playerId') === this.model.get('turn')){
         this.$el.html(this.template({ turn: this.yourText }))
-                .addClass('player'+this.model.get('playerId')+'-turn')
-                .removeClass('player'+Math.abs(this.model.get('playerId')-1)+'-turn');
+                .addClass(this.yourClass)
+                .removeClass(this.oppClass);
       }else{
         this.$el.html(this.template({ turn: this.oppText }))
-                .addClass('player'+Math.abs(this.model.get('playerId')-1)+'-turn')
-                .removeClass('player'+this.model.get('playerId')+'-turn');
+                .addClass(this.oppClass)
+                .removeClass(this.yourClass);
       }
       return this;
     }
