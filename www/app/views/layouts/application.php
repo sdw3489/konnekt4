42b8758ccf84109249264e635d1d07f6c3a7f2cd
Seth Whitaker <sdw3489@gmail.com>
2015-11-28 19:47:06 -0500

    update layout

--- www/app/views/layouts/application.php
+++ www/app/views/layouts/application.php
@@ -1,12 +1,6 @@
-<? $this->load->view('global/header');?>
-
-<div class="container">
-
-	<h1>Your New Site Boilerplate</h1>
-	<h2>CodeIgniter loading RequireJS and Backbone</h2>
+<? $this->load->view('global/head');?>
+<? $this->load->view('global/nav');?>
 
 	<?= $yield; ?>
-
-</div>
 
 <? $this->load->view('global/footer');?>
