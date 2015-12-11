f0c5eb4e180d791954e1190110d6711d930b7b87
Seth Whitaker <sdw3489@gmail.com>
2015-12-11 17:09:47 -0500

    updated Main to extend MY_Controller

--- www/app/controllers/Main.php
+++ www/app/controllers/Main.php
@@ -1,29 +1,25 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Main extends CI_Controller {
+class Main extends MY_Controller {
 
+	protected $models = array('User', 'Stat');
+	protected $helpers = array('url');
 	private $session_id;
 
 	public function __construct(){
 		parent::__construct();
-		$this->load->model('User_model', 'User', TRUE);
-		$this->load->model('Stat_model', 'Stat', TRUE);
 		$this->load->driver('session');
-		$this->load->helper(array('url'));
 		$this->session_id = $this->session->userdata('id');
 	}
 
 	public function index()	{
 		if($this->session_id){
-			$data['title'] = 'Dashboard';
-			$data['bodyClass'] = 'dashboard';
-			$data['id'] = $this->session_id;
-			$data['stats'] = $this->Stat->get($this->session_id);
-			$data['notifications'] = $this->User->getNotifications($_SESSION['id']);
-			$this->load->view('global/head', $data);
-			$this->load->view('global/nav', $data);
-			$this->load->view('dashboard', $data);
-			$this->load->view('global/footer', $data);
+			$this->data['title'] = 'Dashboard';
+			$this->data['bodyClass'] = 'dashboard';
+			$this->data['id'] = $this->session_id;
+			$this->data['stats'] = $this->Stat->get($this->session_id);
+			$this->data['notifications'] = $this->User->getNotifications($_SESSION['id']);
+			$this->view ='dashboard';
 		}else{
 			redirect("/login/");
 		}
