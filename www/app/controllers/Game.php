d6069b8678eeb87632e54d6dd59edb53549433a8
Seth Whitaker <sdw3489@gmail.com>
2015-12-11 14:42:58 -0500

    clean out old api functions from game controller

--- www/app/controllers/Game.php
+++ www/app/controllers/Game.php
@@ -19,58 +19,22 @@ class Game extends MY_Controller {
     }
   }
 
-  public function play($game_Id){
+  public function play($id){
     if($this->session_id){
-      if(isset($game_Id)){
+      if(isset($id)){
         $this->data['title'] = 'Game Board';
         $this->data['bodyClass'] = 'game';
-        $this->data['gameJSON'] = $this->getGameData($game_Id);
+        $this->data['gameJSON'] = json_encode($this->Game->getGameData($id), JSON_NUMERIC_CHECK);
         $this->data['notifications'] = $this->User->getNotifications($this->session_id);
         $this->data['id'] = $this->session_id;
         $this->view = 'game';
       }else{
-        header("Location:/");
+        redirect("/");
       }
     }else{
-      header("Location:/login/");
+      redirect("/login/");
     }
   }
-
-  public function getGameData($game_Id){
-    $data = $this->Game->getGameData($game_Id);
-    return json_encode($data, JSON_NUMERIC_CHECK);
-  }
-
-
-  public function getTurn($game_Id){
-    $data = $this->Game->getTurn($game_Id);
-    echo json_encode($data, JSON_NUMERIC_CHECK);
-  }
-
-  public function changeTurn($game_Id){
-    $this->Game->changeTurn($game_Id);
-  }
-
-  public function updateBoard($game_id){
-    $data = $this->input->post('data');
-    $this->Game->updateBoard($game_id, $data);
-  }
-
-  public function updateLastMove($game_Id){
-    $data = $this->input->post('data');
-    $this->Game->updateLastMove($game_Id, $data);
-  }
-
-  public function getMove($game_Id){
-    $data = $this->Game->getMove($game_Id);
-    echo $data;
-  }
-
-  public function end($game_Id){
-    $data = $this->input->post('data');
-    $this->Game->end($game_Id, $data);
-  }
-
 
   // public function _remap($method, $params = array()) {
   //   if (method_exists($this, $method)){
