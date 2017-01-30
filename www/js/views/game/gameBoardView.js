f7840a5b2a294ff50b764e6e30c6fdeb25acfc03
Seth Whitaker <sdw3489@gmail.com>
2017-01-29 21:05:33 -0700

    port Cell to backbone view

--- www/js/views/game/gameBoardView.js
+++ www/js/views/game/gameBoardView.js
@@ -18,22 +18,31 @@ define([
 
     initialize: function () {
       EventsChannel.on('getMove', this.getMove, this);
+      EventsChannel.on('Cell:clicked', this.placePiece, this);
       this.render();
     },
     render: function () {
       //create a parent to stick board in...
-      var g = utils.createSVG('g',{
-        "id" : "game-board",
-        "stroke" : "blue"
+      this.gameBoard = utils.createSVG('g',{
+        "id" : "game-board"
       });
       //stick g on board
-      this.$el.append(g);
+      this.$el.append(this.gameBoard);
 
       //create the board...
       for(i=0;i<this.model.get('BOARDHEIGHT');i++){//rows i
         this.model.get('boardArr')[i]=new Array();
         for(j=0;j<this.model.get('BOARDWIDTH');j++){//cols j
-          this.model.get('boardArr')[i][j]=new Cell($('#game-board')[0],'cell_'+j+i,75,j,i,this);
+          var cell = new Cell({
+            model: {
+              id : 'cell_'+j+i,
+              size : 75,
+              col : j,
+              row : i
+            }
+          });
+          this.model.get('boardArr')[i][j] = cell;
+          $(this.gameBoard).append(cell.el);
         }
       }
       if(this.model.get('moves').length>0) this.drawMoves();
@@ -54,7 +63,8 @@ define([
        }
     },
     //************************ NEW FUNCTION ***********************/
-    placePiece: function(col){
+    placePiece: function(data){
+      var col = data.col;
       if(this.model.get('active')===true){
         //checks from the bottom of the board up.
         for(var row=this.model.get('boardArr').length-1; row >= 0; row--){
